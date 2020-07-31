f5aa82a00d4601edcd0973e03e887d8f5eb45d61
Rob N ★ <robn@robn.io>
2020-07-31 15:02:32 +1000

    volume-widget: rewrite for pulse, follow events

--- volume-widget/volume.lua
+++ volume-widget/volume.lua
@@ -16,161 +16,155 @@ local gfs = require("gears.filesystem")
 local dpi = require('beautiful').xresources.apply_dpi
 
 local PATH_TO_ICONS = "/usr/share/icons/Arc/status/symbolic/"
-local volume_icon_name="audio-volume-high-symbolic"
-local volume_icon_opacity = 1
-local GET_VOLUME_CMD = 'amixer sget Master'
 
-local volume = {device = '', display_notification = false, notification = nil, delta = 5}
+local volume = {display_notification = false, notification = nil, delta = 5}
 
 function volume:toggle()
-    volume:_cmd('amixer ' .. volume.device .. ' sset Master toggle')
+  volume:_pulsemixer("--toggle-mute")
 end
 
 function volume:raise()
-    volume:_cmd('amixer ' .. volume.device .. ' sset Master ' .. tostring(volume.delta) .. '%+')
+  volume:_pulsemixer('--change-volume +' .. tostring(volume.delta))
 end
 function volume:lower()
-    volume:_cmd('amixer ' .. volume.device .. ' sset Master ' .. tostring(volume.delta) .. '%-')
+  volume:_pulsemixer('--change-volume -' .. tostring(volume.delta))
 end
 
---{{{ Icon and notification update
+function volume:update()
+  volume:_pulsemixer()
+end
 
---------------------------------------------------
---  Set the icon and return the message to display
---  base on sound level and mute
---------------------------------------------------
-local function parse_output(stdout)
-    local level = string.match(stdout, "(%d?%d?%d)%%")
-    if stdout:find("%[off%]") then
-        volume_icon_name="audio-volume-high-symbolic"
-        volume_icon_opacity = 0.2
-        return level.."% <span color=\"red\"><b>Mute</b></span>"
+local calling = false
+function volume:_pulsemixer(cmd)
+  if not cmd and calling then return end
+  calling = true
+  cmd = cmd or ''
+  spawn.easy_async(
+    'pulsemixer ' .. cmd .. ' --get-mute --get-volume',
+    function (stdout, _, _, _)
+      local mute, level = string.match(stdout, "(%d+)\n(%d+)")
+      mute = tonumber(mute) == 1
+      level = tonumber(level)
+
+      if volume.mute ~= nil and volume.level ~= nil and
+         volume.mute == mute and volume.level == level then
+        calling = false
+        return
+      end
+
+      volume.mute = mute
+      volume.level = level
+
+      volume.opacity = volume.mute and 0.2 or 1.0
+
+      volume.icon_name =
+        (volume.level < 25 and "audio-volume-muted-symbolic") or
+        (volume.level < 50 and "audio-volume-low-symbolic") or
+        (volume.level < 75 and "audio-volume-medium-symbolic") or
+                              "audio-volume-high-symbolic"
+
+      volume._update_icon()
+      volume._update_notification()
+
+      calling = false
     end
-    level = tonumber(string.format("% 3d", level))
+  )
+end
 
-    if (level >= 0 and level < 25) then
-        volume_icon_name="audio-volume-muted-symbolic"
-    elseif (level < 50) then
-        volume_icon_name="audio-volume-low-symbolic"
-    elseif (level < 75) then
-        volume_icon_name="audio-volume-medium-symbolic"
+function volume:_update_icon()
+  volume.widget.image = volume.path_to_icons .. volume.icon_name .. ".svg"
+  volume.widget:set_opacity(volume.opacity)
+end
+
+function volume:_update_notification()
+  if volume.display_notification then
+    local text = volume.level .. '%'
+    local image = volume.path_to_icons .. volume.icon_name .. ".svg"
+
+    if not volume.notification then
+      volume.notification = naughty.notify {
+        text = text,
+        icon = image,
+        icon_size = dpi(16),
+        title = "Volume",
+        timeout = 2,
+        hover_timeout = 0.5,
+        width = 200,
+        screen = mouse.screen,
+        destroy = function ()
+          volume.notification = nil
+        end
+      }
     else
-        volume_icon_name="audio-volume-high-symbolic"
+      volume.notification.iconbox.image = image
+      naughty.replace_text(volume.notification, "Volume", text)
     end
-    volume_icon_opacity = 1
-    return level.."%"
-end
 
---------------------------------------------------------
---Update the icon and the notification if needed
---------------------------------------------------------
-local function update_graphic(widget, stdout, _, _, _)
-    local txt = parse_output(stdout)
-    widget.image = volume.path_to_icons .. volume_icon_name .. ".svg"
-    widget:set_opacity(volume_icon_opacity)
-    if volume.display_notification then
-        volume.notification.iconbox.image = volume.path_to_icons .. volume_icon_name .. ".svg"
-        naughty.replace_text(volume.notification, "Volume", txt)
-    end
+    volume.notification.iconbox:set_opacity(volume.opacity)
+  end
 end
-
-local function notif(msg, keep)
-    if volume.display_notification then
-        naughty.destroy(volume.notification)
-        volume.notification= naughty.notify{
-            text =  msg,
-            icon=volume.path_to_icons .. volume_icon_name .. ".svg",
-            icon_size = dpi(16),
-            title = "Volume",
-            position = volume.position,
-            timeout = keep and 0 or 2, hover_timeout = 0.5,
-            width = 200,
-            screen = mouse.screen
-        }
-    end
-end
-
---}}}
 
 local function worker(args)
---{{{ Args
-    local args = args or {}
-    local volume_audio_controller = args.volume_audio_controller or 'pulse'
-    volume.display_notification = args.display_notification or false
-    volume.position = args.notification_position or "top_right"
-    if volume_audio_controller == 'pulse' then
-        volume.device = '-D pulse'
-    end
-    volume.delta = args.delta or 5
-    volume.path_to_icons = args.path_to_icons or PATH_TO_ICONS
-    GET_VOLUME_CMD = 'amixer ' .. volume.device.. ' sget Master'
---}}}
---{{{ Check for icon path
-    if not gfs.dir_readable(volume.path_to_icons) then
-        naughty.notify{
-            title = "Volume Widget",
-            text = "Folder with icons doesn't exist: " .. volume.path_to_icons,
-            preset = naughty.config.presets.critical
-        }
-        return
-    end
---}}}
---{{{ Widget creation
-    volume.widget = wibox.widget {
-        {
-            id = "icon",
-            image = volume.path_to_icons .. "audio-volume-muted-symbolic.svg",
-            resize = false,
-            widget = wibox.widget.imagebox,
-        },
-        layout = wibox.container.margin(_, _, _, 3),
-        set_image = function(self, path)
-            self.icon.image = path
-        end
+  local args = args or {}
+  volume.display_notification = args.display_notification or false
+  volume.position = args.notification_position or "top_right"
+  volume.delta = args.delta or 5
+  volume.path_to_icons = args.path_to_icons or PATH_TO_ICONS
+
+  -- Check for icon path
+  if not gfs.dir_readable(volume.path_to_icons) then
+    naughty.notify {
+      title = "Volume Widget",
+      text = "Folder with icons doesn't exist: " .. volume.path_to_icons,
+      preset = naughty.config.presets.critical
     }
---}}}
---{{{ Spawn functions
-    function volume:_cmd(cmd)
-        notif("")
-        spawn.easy_async(cmd, function(stdout, stderr, exitreason, exitcode)
-            update_graphic(volume.widget, stdout, stderr, exitreason, exitcode)
-        end)
-    end
+    return
+  end
 
-    local function show()
-        spawn.easy_async(GET_VOLUME_CMD,
-        function(stdout, _, _, _)
-        txt = parse_output(stdout)
-        notif(txt, true)
-        end
-        )
+  volume.widget = wibox.widget {
+    {
+      id = "icon",
+      image = volume.path_to_icons .. "audio-volume-muted-symbolic.svg",
+      resize = false,
+      widget = wibox.widget.imagebox,
+    },
+    layout = wibox.container.margin(_, _, _, 3),
+    set_image = function(self, path)
+      self.icon.image = path
     end
---}}}
---{{{ Mouse event
-    --[[ allows control volume level by:
-    - clicking on the widget to mute/unmute
-    - scrolling when cursor is over the widget
-    ]]
-    volume.widget:connect_signal("button::press", function(_,_,_,button)
-        if (button == 4)     then volume.raise()
-        elseif (button == 5) then volume.lower()
-        elseif (button == 1) then volume.toggle()
-        end
+  }
+
+  -- mouse handler, click to toggle mute, scroll to adjust
+  volume.widget:connect_signal("button::press", function(_,_,_,button)
+    if (button == 4)     then volume.raise()
+    elseif (button == 5) then volume.lower()
+    elseif (button == 1) then volume.toggle()
+    end
+  end)
+
+  -- show notification on hover
+  if volume.display_notification then
+    volume.widget:connect_signal("mouse::enter", function()
+      if not volume.notification then
+        volume._update_notification()
+      end
     end)
-    if volume.display_notification then
-        volume.widget:connect_signal("mouse::enter", function() show() end)
-        volume.widget:connect_signal("mouse::leave", function() naughty.destroy(volume.notification) end)
-    end
---}}}
+    volume.widget:connect_signal("mouse::leave", function() naughty.destroy(volume.notification) end)
+  end
 
---{{{ Set initial icon
-    spawn.easy_async(GET_VOLUME_CMD, function(stdout, stderr, exitreason, exitcode)
-        parse_output(stdout)
-        volume.widget.image = volume.path_to_icons .. volume_icon_name .. ".svg"
-    end)
---}}}
+  -- setup monitor
+  spawn.with_line_callback('pipesig pactl subscribe', {
+    stdout = function(line)
+      if line == "Event 'change' on sink #0" then
+        volume.update()
+      end
+    end,
+  })
 
-    return volume.widget
+  -- set initial icon
+  volume.update()
+
+  return volume.widget
 end
 
 return setmetatable(volume, { __call = function(_, ...) return worker(...) end })
